53b5f54f64695ef4784cbd82d18cc7580327a210
marcocampione683 <52001527+marcocampione683@users.noreply.github.com>
2021-08-10 20:25:35 +0200

    Update datiUtente.sql

--- db/datiUtente.sql
+++ db/datiUtente.sql
@@ -3,5 +3,5 @@ Francesca	Filippone	1986-05-15	FrancescaFilippone@gmail.com	YzR2NzhkIQ==	2018-12
 Gerardo	Pagano	1990-12-15	GerryPag@libero.it	MTAzZjJ2Xw==	2019-01-15	utente	1
 Stefania	Di Vece	2012-01-16	Stef64@hotmail.it	MnYxZjVCMA==	2017-08-19	utente	1
 Gianluca	Mastrogiovanni	1976-09-12	Mastro_G@hotmail.it	MWQyZjAuVQ==	2020-01-15	utente	2
-Giovanni	Visone	1995-09-12	gio@libero.it	Z2lvdmFubmk=	2020-05-28	admin	0
-Marco	Campione	1993-07-19	marko.93_@hotmail.it	bWFyY285Mw==	2020-05-28	admin	0
+Giovanni	Visone	1995-09-12	gioll@libero.it	Z2lvdmFubmk=	2020-05-28	admin	0
+Marco	Campione	1993-07-19	marco@hotmail.it	bWFyY285Mw==	2020-05-28	admin	0
