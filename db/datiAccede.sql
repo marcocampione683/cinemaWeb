6ed394c7c6a608a3bc8710f3e12bbe60ec92e0cf
marcocampione683 <52001527+marcocampione683@users.noreply.github.com>
2021-08-10 20:27:01 +0200

    Delete db directory

--- db/datiAccede.sql
+++
@@ -1,7 +0,0 @@
-MarioRossi@libero.it	A4	prenotato
-MarioRossi@libero.it	B7	prenotato
-FrancescaFilippone@gmail.com	C3	acquistato
-GerryPag@libero.it	B1	prenotato
-Stef64@hotmail.it	C9	acquistato
-Mastro_G@hotmail.it	A7	prenotato
-Mastro_G@hotmail.it	B5	prenotato
--- db/datiUtente.sql
+++
@@ -1,7 +0,0 @@
-Mario	Rossi	1995-05-30	MarioRossi@libero.it	YzMxNGQ1Pw==	2019-05-16	utente	2
-Francesca	Filippone	1986-05-15	FrancescaFilippone@gmail.com	YzR2NzhkIQ==	2018-12-03	utente	1
-Gerardo	Pagano	1990-12-15	GerryPag@libero.it	MTAzZjJ2Xw==	2019-01-15	utente	1
-Stefania	Di Vece	2012-01-16	Stef64@hotmail.it	MnYxZjVCMA==	2017-08-19	utente	1
-Gianluca	Mastrogiovanni	1976-09-12	Mastro_G@hotmail.it	MWQyZjAuVQ==	2020-01-15	utente	2
-Giovanni	Visone	1995-09-12	gioll@libero.it	Z2lvdmFubmk=	2020-05-28	admin	0
-Marco	Campione	1993-07-19	marco@hotmail.it	bWFyY285Mw==	2020-05-28	admin	0
